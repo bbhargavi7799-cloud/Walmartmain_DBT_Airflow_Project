afc54453f81cba69d47183e2ae601314f9b09d9b
Bhargavi Bade <b.bhargavi7799@gmail.com>
2026-09-17 14:16:21 -0400

    airflow

--- airflow/dags/orchestrate.py
+++ airflow/dags/orchestrate.py
@@ -1,12 +1,52 @@
 from airflow.sdk import dag, task
 from airflow.operators.bash import BashOperator
+from databricks.sdk import WorkspaceClient
+import os
+import time
+from databricks.sdk.service.jobs import RunLifeCycleState, RunResultState
+import pendulum
 
-@dag
+
+
+@dag(
+              dag_id="orchestrate",
+              schedule="0 11 * * *",  # Run daily at 11
+              catchup=False,
+              start_date=pendulum.datetime(year=2026, month=6, day=18, tz="Canada/Eastern")
+)
 def orchestrate():
 
     @task
-    def ingest_cdc():
-        return "CDC data ingested"
+    def ingest_cdc():     
+
+                ws = WorkspaceClient(
+                host=os.environ["DATABRICKS_HOST"],
+                token=os.environ["DATABRICKS_TOKEN"]
+                )
+
+                job_trigger = ws.jobs.run_now(job_id=964818441363594)
+
+
+                while True:
+                        job_run = ws.jobs.get_run(job_trigger.run_id)
+
+                        print(f"Job run status: {job_run.state.life_cycle_state}, result state: {job_run.state.result_state}")
+
+                        if job_run.state.life_cycle_state in [
+                                RunLifeCycleState.TERMINATED,
+                                RunLifeCycleState.SKIPPED,
+                                RunLifeCycleState.INTERNAL_ERROR,
+                        ]:
+                                if job_run.state.result_state == RunResultState.SUCCESS:
+                                        print("Job completed successfully.")
+                                        break
+                                else:
+                                        raise Exception(f"Job failed with state: {job_run.state.result_state}")
+
+                        time.sleep(5)  # Wait for 10 seconds before checking the job status again
+
+                return "CDC ingestion completed successfully."
+          
 
     @task.bash
     def clean_target():
@@ -44,7 +84,7 @@ def orchestrate():
 
     gold_dimensions = BashOperator(
             task_id='gold_dimensions',
-            bash_command='cd /opt/airflow/walmartmain_project && dbt snapshots'
+            bash_command='cd /opt/airflow/walmartmain_project && dbt snapshot'
     )
 
     gold_facts = BashOperator(
